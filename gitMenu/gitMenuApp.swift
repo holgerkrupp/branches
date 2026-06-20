30c57c9e392b2f97500a9b214de2a884637424f0
Holger Krupp <email@holgerkrupp.de>
2026-06-20 19:16:09 +0200

    Initial Commit

---
+++ gitMenu/gitMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitMenuApp.swift
+//  gitMenu
+//
+//  Created by Holger Krupp on 20.06.26.
+//
+
+import SwiftUI
+
+@main
+struct gitMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
